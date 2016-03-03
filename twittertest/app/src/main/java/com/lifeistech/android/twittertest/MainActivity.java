84ad4d72f0397a38cbe6217fc79ef0c9fe42c26f
shingoi49 <du_bist_so_schon49@yahoo.co.jp>
2016-03-03 23:37:56 +0900

    new version

---
+++ twittertest/app/src/main/java/com/lifeistech/android/twittertest/MainActivity.java
@@ -0,0 +1,333 @@
+package com.lifeistech.android.twittertest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final List<String> READ_PERMISSIONS =
+            Arrays.asList("email", "user_birthday", "user_friends");
+
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String PICTURE = "picture";
+    private static final String EMAIL = "email";
+    private static final String BIRTHDAY = "birthday";
+    private static final String GENDER = "gender";
+    private static final String REQUEST_FIELDS =
+            TextUtils.join(",", new String[]{ID, NAME, PICTURE, EMAIL, BIRTHDAY, GENDER});
+    private static final String FIELDS = "fields";
+    private static final String MESSAGE = "message";
+
+
+    @Bind(R.id.btn_twitter)TextView btn_twitter;
+    private CallbackManager callbackManager;
+    private AccessTokenTracker accessTokenTracker;
+    private ProfileTracker profileTracker;
+    private LoginButton loginButton;
+
+    boolean tLoginFlag = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        Log.e(TAG, "onCreate");
+
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        loginButton = (LoginButton)findViewById(R.id.facebook_login);
+        loginButton.setReadPermissions("user_posts");
+        initFacebook();
+        initToolbar();
+    }
+    /*
+    @OnClick(R.id.btn_getfeed) void onClickBtnGetfeed(){
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/feed",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        Log.d(TAG,response.toString());
+                        JSONObject json = response.getJSONObject();
+                        Log.d(TAG,json.toString());
+                        try{
+                            String message = json.getString(MESSAGE);
+                            Log.d(TAG, String.valueOf(message));
+                        }catch (JSONException e){
+                            Log.e(TAG, e.getMessage());
+                        }
+                    }
+                }
+        ).executeAsync();
+    }*/
+
+    private void initFacebook() {
+
+        callbackManager = CallbackManager.Factory.create();
+
+
+        LoginManager.getInstance()
+                .registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Log.e(TAG, "onSuccess");
+                        requestUserInfo(loginResult.getAccessToken());
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Log.e(TAG, "onCancel");
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Log.e(TAG, "onError");
+                    }
+                });
+
+        accessTokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                       AccessToken currentAccessToken) {
+                // On AccessToken changes fetch the new profile which fires the event on
+                // the ProfileTracker if the profile is different
+                Profile.fetchProfileForCurrentAccessToken();
+            }
+        };
+
+        profileTracker = new ProfileTracker() {
+            @Override protected void onCurrentProfileChanged(Profile oldProfile, Profile currentProfile) {
+                setProfile(currentProfile);
+            }
+        };
+
+        // Ensure that our profile is up to date
+        Profile.fetchProfileForCurrentAccessToken();
+        setProfile(Profile.getCurrentProfile());
+    }
+
+    private void requestUserInfo(AccessToken accessToken) {
+        GraphRequest request =
+                GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        Log.e(TAG, "response: " + response.toString());
+                        JSONObject json = response.getJSONObject();
+                        renderView(json);
+                    }
+                });
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),"/me/feed",null, HttpMethod.GET,new GraphRequest.Callback() {
+            public void onCompleted(GraphResponse response) {
+                JSONObject json = response.getJSONObject();
+                Log.i(TAG,json.toString());
+                try{
+                    String message = json.getString(MESSAGE);
+                    Log.i(TAG, String.valueOf(message));
+                }catch (JSONException e){
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        }
+        ).executeAsync();
+        Bundle parameters = new Bundle();
+        parameters.putString(FIELDS, REQUEST_FIELDS);
+        request.setParameters(parameters);
+        request.executeAsync();
+        ShareActivity.start(this);
+    }
+
+    private void renderView(JSONObject json) {
+        try {
+            final long id = json.getLong(ID);
+            final String name = json.getString(NAME);
+            final String email = json.getString(EMAIL);
+            final String picture = json.getJSONObject("picture").getJSONObject("data").getString("url");
+            final String birthday = json.getString(BIRTHDAY);
+            final String gender = json.getString(GENDER);
+
+            //txtId.setText(String.valueOf(id));
+            //txtName.setText(name);
+            //txtBirthday.setText(birthday);
+            //txtGender.setText(gender);
+            //txtEmail.setText(email);
+            Log.e(TAG, "picture: " + picture);
+            //ImageLoader.getInstance().displayImage(picture, imgProfile);
+        } catch (JSONException e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private void setProfile(Profile profile) {
+        if (profile != null) {
+            //txtId.setText(String.valueOf(profile.getId()));
+            //txtName.setText(profile.getName());
+            //ImageLoader.getInstance().displayImage(profile.getProfilePictureUri(100, 100).toString(), imgProfile);
+        } else {
+            Log.e(TAG, "profile is null.");
+        }
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        accessTokenTracker.stopTracking();
+        profileTracker.stopTracking();
+    }
+
+    @OnClick(R.id.btn_login) void onClickBtnLogin() {
+        Log.e(TAG, "onClickBtnLogin");
+        LoginManager manager = LoginManager.getInstance();
+        manager.logInWithReadPermissions(this, READ_PERMISSIONS);
+    }
+
+    @OnClick(R.id.btn_twitter) void onClickBtnTwitterLogin() {
+
+        //ShareActivity.start(this);
+        if(tLoginFlag == false){
+            tLoginFlag = true;
+            btn_twitter.setText("Twitter_TL");
+            Intent intent = new Intent(this,TwitterApp.class);
+            startActivity(intent);
+      /*
+      if (!TwitterUtils.hasAccessToken(this)) {
+        Intent intent = new Intent(this, TwitterOathSignIn.class);
+        startActivity(intent);
+      }*/
+        }else{
+            Intent intent = new Intent(this,TwitterApp.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_tweet) {
+            Intent intent = new Intent(this, TwitterApp.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_share) {
+            Intent intent = new Intent(this,ShareActivity.class);
+            startActivity(intent);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ twittertest/app/src/main/java/com/lifeistech/android/twittertest/ShareActivity.java
@@ -0,0 +1,234 @@
+package com.lifeistech.android.twittertest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.share.ShareApi;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.widget.ShareDialog;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * https://developers.facebook.com/docs/sharing/android
+ */
+public class ShareActivity extends FragmentActivity {
+
+  private static final String TAG = ShareActivity.class.getSimpleName();
+  private static final List<String> PUBLISH_PERMISSIONS = Arrays.asList("publish_actions");
+  private static final String SAMPLE_SHARE_URL = "http://google.com";
+  private static final String SAMPLE_SHARE_TITLE = "test_title";
+  private static final String SAMPLE_SHARE_DESCRIPTION = "test_description";
+  private static final String SAMPLE_IMAGE_URL = "";
+  private static final String MESSAGE = "message";
+  private ArrayList msgList = new ArrayList();
+
+  @Bind(R.id.txt_share_auto_result) TextView txtShareAutoResult;
+  @Bind(R.id.listView)ListView feedview;
+
+  private CallbackManager callbackManager;
+
+  static void start(Context context) {
+    Intent intent = new Intent(context, ShareActivity.class);
+    context.startActivity(intent);
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_share);
+    ButterKnife.bind(this);
+    Log.e(TAG, "onCreate");
+
+
+    initFacebook();
+    initToolbar();
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    // Logs 'install' and 'app activate' App Events.
+    AppEventsLogger.activateApp(this);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    // Logs 'app deactivate' App Event.
+    AppEventsLogger.deactivateApp(this);
+  }
+
+  private void initFacebook() {
+    FacebookSdk.sdkInitialize(getApplicationContext());
+
+    callbackManager = CallbackManager.Factory.create();
+    LoginManager.getInstance()
+        .registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+          @Override
+          public void onSuccess(LoginResult loginResult) {
+            Log.e(TAG, "onSuccess");
+            shareToFacebookFeed();
+          }
+
+          @Override
+          public void onCancel() {
+            Log.e(TAG, "onCancel");
+          }
+
+          @Override
+          public void onError(FacebookException exception) {
+            Log.e(TAG, "onError");
+          }
+        });
+  }
+
+  private void initToolbar() {
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    toolbar.setTitle(R.string.share);
+    toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+    toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        onBackPressed();
+      }
+    });
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    callbackManager.onActivityResult(requestCode, resultCode, data);
+  }
+
+  @OnClick(R.id.btn_getfeed) void onClickBtnGetfeed(){
+    final Intent intent = new Intent(this,ShareActivity.class);
+    new GraphRequest(
+            AccessToken.getCurrentAccessToken(),
+            "/me/feed",
+            null,
+            HttpMethod.GET,
+            new GraphRequest.Callback() {
+              public void onCompleted(GraphResponse response) {
+                JSONObject json = response.getJSONObject();
+                try{
+                  JSONArray jsons = json.getJSONArray("data");
+                  for(int i = 0; i < jsons.length(); i++){
+                    JSONObject fData = jsons.getJSONObject(i);
+                    if(fData.has("message")){
+                      msgList.add(fData.getString("message") + "\n");
+                    }
+                  }
+                  intent.putStringArrayListExtra("feedlist",msgList);
+                  startActivity(intent);
+                }catch (JSONException e){
+                  Log.e(TAG, e.getMessage());
+                }
+              }
+            }
+    ).executeAsync();
+  }
+
+  @OnClick(R.id.btn_share_dialog) void onClickBtnShareDialog() {
+    if (ShareDialog.canShow(ShareLinkContent.class)) {
+      ShareLinkContent content =
+          new ShareLinkContent.Builder()//.setContentUrl(Uri.parse(SAMPLE_SHARE_URL))
+     //         .setContentTitle(SAMPLE_SHARE_TITLE)
+     //         .setContentDescription(SAMPLE_SHARE_DESCRIPTION)
+              .build();
+      ShareDialog.show(this,content);
+    }
+  }
+/*
+  @OnClick(R.id.btn_share_auto) void onClickBtnShareAuto() {
+    LoginManager manager = LoginManager.getInstance();
+    manager.logInWithPublishPermissions(this, PUBLISH_PERMISSIONS);
+  }
+
+  @OnClick(R.id.btn_share_photo_auto) void onClickBtnSharePhotoAuto() {
+    LoginManager manager = LoginManager.getInstance();
+    manager.logInWithPublishPermissions(this, PUBLISH_PERMISSIONS);
+  }
+*/
+  private void shareToFacebookFeed() {
+    ShareLinkContent content =
+        new ShareLinkContent.Builder().setContentUrl(Uri.parse(SAMPLE_SHARE_URL))
+            .setContentTitle(SAMPLE_SHARE_TITLE)
+            .setContentDescription(SAMPLE_SHARE_DESCRIPTION)
+            .setImageUrl(Uri.parse(SAMPLE_IMAGE_URL))
+            .build();
+    ShareApi.share(content, new FacebookCallback<Sharer.Result>() {
+      @Override public void onSuccess(Sharer.Result result) {
+        Log.e(TAG, "onSuccess");
+        txtShareAutoResult.setText(R.string.share_auto_completed);
+      }
+
+      @Override public void onCancel() {
+        Log.e(TAG, "onCancel");
+        txtShareAutoResult.setText(R.string.share_auto_failed);
+      }
+
+      @Override public void onError(FacebookException e) {
+        Log.e(TAG, "onError");
+        txtShareAutoResult.setText(R.string.share_auto_failed);
+      }
+    });
+  }
+
+  private void sharePhotoToFacebook() {
+    SharePhoto photo = new SharePhoto.Builder().setImageUrl(Uri.parse(SAMPLE_IMAGE_URL)).build();
+    SharePhotoContent content = new SharePhotoContent.Builder().addPhoto(photo)
+        .setContentUrl(Uri.parse(SAMPLE_SHARE_URL))
+        .setRef(SAMPLE_SHARE_TITLE)
+        .build();
+
+    ShareApi.share(content, new FacebookCallback<Sharer.Result>() {
+      @Override public void onSuccess(Sharer.Result result) {
+        Log.e(TAG, "onSuccess");
+        txtShareAutoResult.setText(R.string.share_auto_completed);
+      }
+
+      @Override public void onCancel() {
+        Log.e(TAG, "onCancel");
+        txtShareAutoResult.setText(R.string.share_auto_failed);
+      }
+
+      @Override public void onError(FacebookException e) {
+        Log.e(TAG, "onError");
+        txtShareAutoResult.setText(R.string.share_auto_failed);
+      }
+    });
+  }
+}
